37603d9a646aa0ed1f9752e4b8aa73eeacf72500
Koushik Yerraguntla <koushikyerraguntla@gmail.com>
2025-11-25 11:11:28 +0530

    added seo

--- app/components/Header.js
+++ app/components/Header.js
@@ -93,21 +93,7 @@ export default function Header() {
         </svg>
       ),
     },
-    {
-      name: "Email",
-      href: "mailto:fmcginfluencers@gmail.com",
-      icon: (
-        <svg
-          className="w-7 h-7"
-          fill="currentColor"
-          viewBox="0 0 24 24"
-          aria-hidden="true"
-        >
-          <path d="M1.5 8.67v8.58a3 3 0 003 3h15a3 3 0 003-3V8.67l-8.928 5.493a3 3 0 01-3.144 0L1.5 8.67z" />
-          <path d="M22.5 6.908V6.75a3 3 0 00-3-3h-15a3 3 0 00-3 3v.158l9.714 5.978a1.5 1.5 0 001.572 0L22.5 6.908z" />
-        </svg>
-      ),
-    },
+  
   ];
 
   const scrollToSection = (sectionId) => {
@@ -350,7 +336,7 @@ export default function Header() {
                 </nav>
 
                 {/* Register Button */}
-                <div className="mb-2">
+                <div className="mb-2 pt-3">
                   <a
                     href="/register"
                     className="block w-full bg-primary text-secondary px-6 py-4 rounded-xl hover:bg-primary/90 transition-colors font-bold text-lg text-center"
